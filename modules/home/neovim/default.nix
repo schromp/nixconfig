9b72e66663b8980deb980adce34c788bb461f33b
schromp <lennart.koziolek@stud.uni-due.de>
2023-05-30 15:36:37 +0200

    moved out of .dotfiles folder into base folder

---
+++ modules/home/neovim/default.nix
@@ -0,0 +1,48 @@
+{ config, pkgs, lib, ... }:
+let
+  # installs a vim plugin from git with a given tag / branch
+  # pluginGit = ref: repo: vimUtils.buildVimPluginFrom2Nix {
+  #   pname = "${lib.strings.sanitizeDerivationName repo}";
+  #   version = ref;
+  #   src = builtins.fetchGit {
+  #     url = "https://github.com/${repo}.git";
+  #     ref = ref;
+  #   };
+  # };
+
+  # always installs latest version
+  # plugin = pluginGit "HEAD";
+in
+{
+
+  # dependencies for nvim
+  home.packages = with pkgs; [
+      ripgrep
+      fd
+      fzf
+
+      tree-sitter
+
+      # LSPs
+
+      clang-tools
+      # rnix-lsp
+      nil
+      sumneko-lua-language-server
+      rust-analyzer
+      statix
+      nixpkgs-fmt
+      nil
+   ];
+
+  programs.neovim = {
+    enable = true;
+    defaultEditor = true;
+    withNodeJs = true;
+    withPython3 = true;
+  };
+
+
+  programs.fzf.enable = true;
+
+}

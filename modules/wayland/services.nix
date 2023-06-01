066b6e821241932a92eca9c7a9d718d245fadbac
schromp <lennart.koziolek@stud.uni-due.de>
2023-06-01 15:44:01 +0200

    bootloader working + login things with greetd

--- modules/wayland/services.nix
+++ modules/wayland/services.nix
@@ -33,7 +33,7 @@
     enable = true;
     settings = {
       default_session = {
-        command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --remember-session --remember-user-session --greeting SUP --cmd Hyprland";
+        command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --cmd Hyprland";
         user = "greeter";
       };
     };

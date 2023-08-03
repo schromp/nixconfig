720f647ac78722861a94ba33edcbe08cd7ba3234
schromp <lennart.koziolek@stud.uni-due.de>
2023-08-03 16:55:01 +0200

    glass theme additions

--- presets/rices/glass.nix
+++ presets/rices/glass.nix
@@ -15,21 +15,20 @@ in {
     presets.themes.name = "catppuccin-latte";
 
     home-manager.users.${username} = {
-      wayland.windowManager.hyprland.settings =
-        mkIf (config.modules.user.desktopEnvironment == "hyprland") {
-          decoration = {
-            rounding = 5;
-            multisample_edges = true;
-            blur = true;
-            blur_size = 5;
-            blur_passes = 5;
-            blur_ignore_opacity = true;
-            drop_shadow = true;
-            shadow_range = 30;
-            shadow_render_power = 3;
-            "col.shadow" = "00000099";
-          };
+      wayland.windowManager.hyprland.settings = mkIf (config.modules.user.desktopEnvironment == "hyprland") {
+        decoration = {
+          rounding = 5;
+          multisample_edges = true;
+          blur = true;
+          blur_size = 5;
+          blur_passes = 5;
+          blur_ignore_opacity = true;
+          drop_shadow = true;
+          shadow_range = 30;
+          shadow_render_power = 3;
+          "col.shadow" = "00000099";
         };
+      };
     };
   };
 }

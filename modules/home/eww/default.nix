b47c7b9124242d08c1ed6b099109ed646335420e
schromp <lennart.koziollek@stud.uni-due.de>
2023-08-23 17:57:26 +0200

    updated eww config. very early WIP

--- modules/home/eww/default.nix
+++ modules/home/eww/default.nix
@@ -3,18 +3,19 @@
   programs.eww = {
     enable = true;
     package = inputs.eww.packages.x86_64-linux.eww-wayland;
-    configDir = ./config;
+    configDir = config.lib.file.mkOutOfStoreSymlink ./config;
   };
 
-  # home.packages = with pkgs; [
+  home.packages = with pkgs; [
+    gnused
   #   eww-wayland
   #   pamixer
   #   brightnessctl
-  #   (nerdfonts.override { fonts = [ "JetBrainsMono" ]; })
-  # ];
+    (nerdfonts.override { fonts = [ "JetBrainsMono" ]; })
+  ];
   #
   #
-  # home.file.".config/eww/eww.scss".source = ./eww.scss;
-  # home.file.".config/eww/eww.yuck".source = ./eww.yuck;
+  #home.file.".config/eww/eww.scss".source = ./config/eww.scss;
+  #home.file.".config/eww/eww.yuck".source = ./config/eww.yuck;
 
 }

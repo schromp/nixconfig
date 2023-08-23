9e946b6cc67684c039fdbe1b961462d78ef86873
schromp <lennart.koziollek@stud.uni-due.de>
2023-08-23 17:57:26 +0200

    added treesitter with all grammars

--- modules/home/neovim/default.nix
+++ modules/home/neovim/default.nix
@@ -1,41 +1,43 @@
-{ config, pkgs, lib, ... }:
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}:
 # let
-  # installs a vim plugin from git with a given tag / branch
-  # pluginGit = ref: repo: vimUtils.buildVimPluginFrom2Nix {
-  #   pname = "${lib.strings.sanitizeDerivationName repo}";
-  #   version = ref;
-  #   src = builtins.fetchGit {
-  #     url = "https://github.com/${repo}.git";
-  #     ref = ref;
-  #   };
-  # };
-
-  # always installs latest version
-  # plugin = pluginGit "HEAD";
+# installs a vim plugin from git with a given tag / branch
+# pluginGit = ref: repo: vimUtils.buildVimPluginFrom2Nix {
+#   pname = "${lib.strings.sanitizeDerivationName repo}";
+#   version = ref;
+#   src = builtins.fetchGit {
+#     url = "https://github.com/${repo}.git";
+#     ref = ref;
+#   };
+# };
+# always installs latest version
+# plugin = pluginGit "HEAD";
 # in
 {
-
   # dependencies for nvim
   home.packages = with pkgs; [
-      neovide
+    neovide
 
-      ripgrep
-      fd
-      fzf
-      tree-sitter
+    ripgrep
+    fd
+    fzf
+    tree-sitter
 
-      # LSPs
-      clang-tools
-      nil
-      sumneko-lua-language-server
-      rust-analyzer
-      statix
-      nixpkgs-fmt
-      nil
-      rnix-lsp
-      python311Packages.jedi-language-server
-
-   ];
+    # LSPs
+    clang-tools
+    nil
+    sumneko-lua-language-server
+    rust-analyzer
+    statix
+    nixpkgs-fmt
+    nil
+    rnix-lsp
+    python311Packages.jedi-language-server
+  ];
 
   programs.neovim = {
     enable = true;
@@ -44,6 +46,9 @@
     withPython3 = true;
   };
 
-  programs.fzf.enable = true;
+  programs.neovim.plugins = [
+    pkgs.vimPlugins.nvim-treesitter.withAllGrammars
+  ];
 
+  programs.fzf.enable = true;
 }

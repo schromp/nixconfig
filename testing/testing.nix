755b072c8585d0f537fa8b2bbc741d7bf0b475ff
schromp <lennart.koziollek@stud.uni-due.de>
2023-08-23 17:57:26 +0200

    abondon ship

--- testing/testing.nix
+++ testing/testing.nix
@@ -1,23 +1,38 @@
-{ inputs, lib, config, home-manager, ...}:
+{
+  lib,
+  config,
+  inputs,
+  username,
+  ...
+}:
 with lib; let
   cfg = config.modules.desktop.hyprland;
-  username = "lk";
   hm = inputs.home-manager.nixosModules.home-manager;
-in {
-
-  options.modules.desktop.hyprland.enable = mkEnableOption "Enable Hyprland";
-
-  config = mkIf cfg.enable {
-  
-    # TODO: add options to nixosModules and home-manager modules
-
-    #modules = [ inputs.hyprland.nixosModules.default hm ];
-    modules = [ hm ];
-
-    inputs.home-manager.users.${username} = {
-      modules = [ inputs.hyprland.homeManagerModules.default ];
+  home-manager = {
+    useUserPackages = true;
+    useGlobalPkgs = true;
+    extraSpecialArgs = {
+      inherit inputs; # make flake inputs accessible to home manager
+    };
+    users.${username} = {
+      modules = [inputs.hyprland.homeManagerModules.default];
       wayland.windowManager.hyprland.enable = true;
     };
   };
+in {
+  options.modules.desktop.hyprland.enable = mkEnableOption "Enable Hyprland";
 
+  config = with home-manager;
+    mkIf cfg.enable {
+      # TODO: add options to nixosModules and home-manager modules
+      modules = [
+        hm
+        home-manager
+      ];
+
+      environment.systemPackages = with pkgs; [
+        discord
+      ];
+
+    };
 }

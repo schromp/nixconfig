18728057a461b984e4520a910dd8f6e554b94363
schromp <lennart.koziollek@stud.uni-due.de>
2023-08-23 17:59:53 +0200

    work machine testing

--- hosts/default.nix
+++ hosts/default.nix
@@ -21,28 +21,29 @@
       ];
       specialArgs = {inherit inputs;};
     };
-  mkDarwinSystem = system: hostname:
-    nix-darwin.lib.darwinSystem {
-      # nixpkgs.hostPlatform = "${system}";
-      system = "${system}";
-      modules = [
-        {
-          config.modules.system = {
-            hostname = hostname;
-            architecture = system;
-          };
-        }
-        home-manager.darwinModules.home-manager
-        ./${hostname}
-      ];
-      specialArgs = {inherit inputs;};
-    };
+  # mkDarwinSystem = system: hostname:
+  #   nix-darwin.lib.darwinSystem {
+  #     # nixpkgs.hostPlatform = "${system}";
+  #     system = "${system}";
+  #     modules = [
+  #       {
+  #         config.modules.system = {
+  #           hostname = hostname;
+  #           architecture = system;
+  #         };
+  #       }
+  #       home-manager.darwinModules.home-manager
+  #       ./${hostname}
+  #     ];
+  #     specialArgs = {inherit inputs;};
+  #   };
 in {
   nixosSystems = {
     tower = mkNixosSystem "x86_64-linux" "tower";
+    cake = mkNixosSystem "aarch64" "cake";
   };
 
-  darwinSystems = {
-    viajar = mkDarwinSystem "x86_64-darwin" "viajar";
-  };
+  # darwinSystems = {
+  #   viajar = mkDarwinSystem "x86_64-darwin" "viajar";
+  # };
 }
--- hosts/viajar/features.nix
+++ hosts/viajar/features.nix
@@ -10,7 +10,7 @@
         username = "lennartkoziollek";
       };
       system = {
-        nvidia = false;
+
       };
     };
   };

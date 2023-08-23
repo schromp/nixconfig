48c362e3ec83150bb2678fa6e390769d7136da38
schromp <lennart.koziollek@stud.uni-due.de>
2023-08-23 17:59:53 +0200

    added eww systemd service

--- modules/desktop/eww/default.nix
+++ modules/desktop/eww/default.nix
@@ -1,13 +1,16 @@
 {
   config,
   lib,
-  inputs,
   pkgs,
   ...
 }:
 with lib; let
   username = config.modules.user.username;
   cfg = config.modules.programs.eww;
+  pkg =
+    if config.modules.user.displayServerProtocol == "wayland"
+    then pkgs.eww-wayland
+    else pkgs.eww;
 in {
   options.modules.programs.eww = {
     enable = mkEnableOption "Enable Eww";
@@ -16,12 +19,7 @@ in {
   config = mkIf cfg.enable {
     # TODO: add systemd service that starts after compositor
     home-manager.users.${username} = {
-      programs.eww = let 
-        pkg = if config.modules.user.displayServerProtocol == "wayland" 
-        then inputs.eww.packages.x86_64-linux.eww-wayland
-        else
-          pkgs.eww;
-      in {
+      programs.eww = {
         enable = true;
         package = pkg;
         # configDir = config.lib.file.mkOutOfStoreSymlink ./config;
@@ -30,11 +28,25 @@ in {
 
       home.packages = with pkgs; [
         gnused
-        #   eww-wayland
         #   pamixer
         #   brightnessctl
         (nerdfonts.override {fonts = ["JetBrainsMono"];})
       ];
+
+      systemd.user.services.eww = {
+        Unit = {
+          Description = "Eww Daemon";
+          # not yet implemented
+          # PartOf = ["tray.target"];
+          PartOf = ["graphical-session.target"];
+        };
+        Service = {
+          # Environment = "PATH=/run/wrappers/bin:${lib.makeBinPath dependencies}";
+          ExecStart = "${pkg}/bin/eww daemon --no-daemonize";
+          Restart = "on-failure";
+        };
+        Install.WantedBy = ["graphical-session.target"];
+      };
     };
   };
 }

6f7016eaf2e3d92ba9ac39d3d8c7c74d35a6f500
schromp <lennart.koziollek@stud.uni-due.de>
2023-08-23 17:57:26 +0200

    english german keyboard layout

--- modules/home/hyprland/default.nix
+++ modules/home/hyprland/default.nix
@@ -17,6 +17,22 @@
 
   xdg.configFile."hypr/hyprpaper.conf".text = builtins.readFile ./hyprpaper.conf;
 
+  # umlaute into keyboard
+  xdg.configFile."xkb/symbols/us-german-umlaut".text = ''
+    default partial alphanumeric_keys
+    xkb_symbols "basic" {
+      include "us(altgr-intl)"
+      include "level3(caps_switch)"
+      name[Group1] = "English (US, international with German umlaut)";
+      key <AD03> { [ e, E, EuroSign, cent ] };
+      key <AD07> { [ u, U, udiaeresis, Udiaeresis ] };
+      key <AD09> { [ o, O, odiaeresis, Odiaeresis ] };
+      key <AC01> { [ a, A, adiaeresis, Adiaeresis ] };
+      key <AC02> { [ s, S, ssharp ] };
+    };
+
+  '';
+
   # TODO start the services i need
   services.dunst.enable = true;
 }

cce96eb797a3386c8d9122a5c6dd0442aea5b44d
schromp <lennart.koziollek@stud.uni-due.de>
2023-08-23 17:59:53 +0200

    nvidia open driver

--- modules/system/nvidia.nix
+++ modules/system/nvidia.nix
@@ -4,7 +4,7 @@ with lib; let
 in {
   config = mkIf cfg {
     services.xserver.videoDrivers = ["nvidia"];
-    #hardware.nvidia.open = true;
+    hardware.nvidia.open = true;
     #hardware.nvidia.nvidiaSettings = true;
     #hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.stable;
     hardware.nvidia.modesetting.enable = true;

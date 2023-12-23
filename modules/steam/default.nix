246e91df6bdeecbb0142c6e52dbc6bc106f27c0f
Lennart Koziollek <lennart.koziollek@stud.uni-due.de>
2023-12-23 12:24:14 +0100

    fix gamescope in steam

--- modules/steam/default.nix
+++ modules/steam/default.nix
@@ -12,6 +12,21 @@ in
   config = mkIf cfg.enable {
     programs.steam = {
       enable = true;
+      package = pkgs.steam.override {
+        extraPkgs = pkgs:
+          with pkgs; [
+            keyutils
+            libkrb5
+            libpng
+            libpulseaudio
+            libvorbis
+            stdenv.cc.cc.lib
+            xorg.libXcursor
+            xorg.libXi
+            xorg.libXinerama
+            xorg.libXScrnSaver
+          ];
+      };
     };
 
     environment.systemPackages = with pkgs; [ mangohud ];

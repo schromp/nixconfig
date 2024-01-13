ec453c2b22211ab222a93c431013713aa504f1cf
Lennart Koziollek <lennart.koziollek@stud.uni-due.de>
2024-01-13 21:05:14 +0100

    added obsidian + misc packages

--- hosts/tower/extra-packages.nix
+++ hosts/tower/extra-packages.nix
@@ -1,11 +1,15 @@
 {
   pkgs,
   config,
+  lib,
   ...
 }: let
   username = config.modules.user.username;
 in {
   # here you can add packages specific to your setup.
+
+  nixpkgs.config.permittedInsecurePackages = lib.optional (pkgs.obsidian.version == "1.4.16") "electron-25.9.0";
+
   environment.systemPackages = with pkgs; [
     pantheon.elementary-iconbrowser
     texliveFull
@@ -18,6 +22,11 @@ in {
     discord
     pcmanfm
     helvum
+    vlc
+    vscodium
+    obsidian
+
+    alsa-utils
 
     audacity
     soundux

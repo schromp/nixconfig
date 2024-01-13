6dcbd5d589506f91bb9d0c44ad165f5a971581a4
Lennart Koziollek <lennart.koziollek@stud.uni-due.de>
2024-01-13 21:06:45 +0100

    test pipewire changes

--- modules/pipewire/default.nix
+++ modules/pipewire/default.nix
@@ -11,16 +11,18 @@ in {
   };
 
   config = mkIf cfg.enable {
+    security.rtkit.enable = true;
     services = {
       pipewire = {
         enable = true;
+        audio.enable = true;
         alsa = {
           enable = true;
           support32Bit = true;
         };
         wireplumber.enable = true;
         pulse.enable = true;
-        # jack.enable = true;
+        jack.enable = true;
       };
     };
   };

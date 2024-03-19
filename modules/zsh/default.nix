370adefd92c28dce9bc3d60404435ca97817eb46
Lennart Koziollek <lennart.koziollek@stud.uni-due.de>
2024-03-19 18:27:35 +0100

    WIP shell prompt

--- modules/zsh/default.nix
+++ modules/zsh/default.nix
@@ -15,16 +15,19 @@ in {
   config = mkIf cfg.enable {
     # for zsh autocompletions on systemlevel
     environment.pathsToLink = ["/share/zsh"];
-    environment.systemPackages = with pkgs; [fzf eza];
+    environment.systemPackages = with pkgs; [fzf eza killall];
 
-    programs.zsh.enable = true;
+    programs.zsh = {
+      enable = true;
+    };
 
     programs.fzf.keybindings = true;
 
     home-manager.users.${username} = {
-      imports = [./starship.nix];
+      # imports = [./starship.nix];
       programs.zsh = {
         enable = true;
+        dotDir = ".config/zsh";
         enableAutosuggestions = true;
         enableCompletion = true;
         syntaxHighlighting.enable = true;
@@ -60,7 +63,54 @@ in {
           enable = true;
           plugins = ["sudo" "web-search" "git"];
           extraConfig = ''
-            DISABLE_MAGIC_FUNCTIONS="true"
+            DISABLE_MAGIC_FUNCTIONS="true" # This fixes slow pasting
+
+            # --- Prompt ---
+            # Autoload zsh's `add-zsh-hook` and `vcs_info` functions
+            # (-U autoload w/o substition, -z use zsh style)
+            autoload -Uz add-zsh-hook vcs_info
+
+            # Set prompt substitution so we can use the vcs_info_message variable
+            setopt prompt_subst
+
+
+
+            # Run the `vcs_info` hook to grab git info before displaying the prompt
+            add-zsh-hook precmd vcs_info
+
+
+            # Style the vcs_info message
+            zstyle ':vcs_info:*' enable git
+            zstyle ':vcs_info:git*' formats '%b%u%c'
+            # Format when the repo is in an action (merge, rebase, etc)
+            zstyle ':vcs_info:git*' actionformats '%F{14}⏱ %*%f'
+            zstyle ':vcs_info:git*' unstagedstr '*'
+            zstyle ':vcs_info:git*' stagedstr '+'
+            # This enables %u and %c (unstaged/staged changes) to work,
+            # but can be slow on large repos
+            zstyle ':vcs_info:*:*' check-for-changes true
+
+            COL_NAME="cba6f7"
+            COL_DIR="f2cdcd"
+            COL_GIT="fab387"
+
+            NEWLINE=$'\n'
+            source ~/.config/zsh/prompt.sh
+
+            function getColors() {
+              unset COL_NAME COL_DIR COL_GIT
+              source ~/.config/zsh/prompt.sh
+            }
+            add-zsh-hook precmd getColors
+
+
+            PROMPT="''${NEWLINE}%F{#$COL_NAME}%n%f %F{#$COL_DIR}%3~%f %F{#$COL_GIT}''${vcs_info_msg_0_}%f %(?..%B%F{red}(%?%)%f%b)''${NEWLINE}> "
+
+            ${
+              if config.modules.programs.themer.enable
+              then "source ~/.config/zsh/prompt.sh"
+              else ""
+            }
           '';
         };
       };

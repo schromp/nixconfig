54cc19ff4e768b2d0126b724cab8510697152926
Lennart Koziollek <lennart.koziollek@stud.uni-due.de>
2024-03-31 22:56:31 +0200

    wacom

--- modules/input/wacom.nix
+++ modules/input/wacom.nix
@@ -8,13 +8,13 @@ with lib; let
   cfg = config.modules.system.wacom;
 in {
   config = mkIf cfg {
-    services.xserver.wacom.enable = true;
+    # services.xserver.wacom.enable = true;
     hardware.opentabletdriver.enable = true;
 
-    environment.systemPackages = with pkgs; [
-      xf86_input_wacom
-      libwacom
-      wacomtablet
-    ];
+    # environment.systemPackages = with pkgs; [
+    #   xf86_input_wacom
+    #   libwacom
+    #   wacomtablet
+    # ];
   };
 }

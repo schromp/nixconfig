b667b6f1060856f444ae18efc29cb4a13f8a7ebd
Lennart Koziollek <lennart.koziollek@stud.uni-due.de>
2024-04-11 12:35:21 +0200

    some sway laptop changes

--- modules/sway/default.nix
+++ modules/sway/default.nix
@@ -10,6 +10,7 @@ with lib; let
   username = config.modules.user.username;
   enabled = config.modules.user.desktopEnvironment == "sway";
   appRunner = config.modules.user.appRunner;
+  screenshotTool = config.modules.user.screenshotTool;
   modifier = "Mod4";
 in {
   config = mkIf enabled {
@@ -23,8 +24,11 @@ in {
         };
         systemd.enable = true;
         config = {
-          # output = {
-          # };
+          output = {
+            eDP-1 = {
+              scale = "1.5";
+            };
+          };
           modifier = "${modifier}";
           menu = "walker";
           keybindings = lib.mkOptionDefault {
@@ -39,10 +43,38 @@ in {
               accel_profile = "flat";
               pointer_accel = "0.3";
               xkb_layout = "us-german-umlaut";
+              natural_scroll = "enabled";
+              tap = "enabled";
             };
           };
         };
+        extraConfig = ''
+          bindgesture swipe:right workspace prev
+          bindgesture swipe:left workspace next
+        '';
       };
+
+      home.packages = with pkgs; [
+        brightnessctl # change this to light probably
+        wl-clipboard
+        swaylock-effects
+        swayidle
+        libnotify
+        xwaylandvideobridge
+
+        slurp
+        grim
+
+        (
+          if screenshotTool == "grimblast"
+          then inputs.hyprland-contrib.packages.${pkgs.system}.grimblast
+          else if screenshotTool == "satty"
+          then satty
+          else if screenshotTool == "swappy"
+          then swappy
+          else null
+        )
+      ];
     };
   };
 }

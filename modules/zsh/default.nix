1bd12d17c51165ab215a8d53f63ced89cc7389fc
Lennart Koziollek <lennart.koziollek@stud.uni-due.de>
2024-05-24 15:47:03 +0200

    fix zsh prompt recoloring

--- modules/zsh/default.nix
+++ modules/zsh/default.nix
@@ -69,27 +69,10 @@ in {
             # --- Prompt ---
             # Autoload zsh's `add-zsh-hook` and `vcs_info` functions
             # (-U autoload w/o substition, -z use zsh style)
-            autoload -Uz add-zsh-hook vcs_info
+            # autoload -Uz add-zsh-hook vcs_info
 
             # Set prompt substitution so we can use the vcs_info_message variable
             setopt prompt_subst
-
-
-
-            # Run the `vcs_info` hook to grab git info before displaying the prompt
-            add-zsh-hook precmd vcs_info
-
-
-            # Style the vcs_info message
-            zstyle ':vcs_info:*' enable git
-            zstyle ':vcs_info:git*' formats '%b%u%c'
-            # Format when the repo is in an action (merge, rebase, etc)
-            zstyle ':vcs_info:git*' actionformats '%F{14}⏱ %*%f'
-            zstyle ':vcs_info:git*' unstagedstr '*'
-            zstyle ':vcs_info:git*' stagedstr '+'
-            # This enables %u and %c (unstaged/staged changes) to work,
-            # but can be slow on large repos
-            zstyle ':vcs_info:*:*' check-for-changes true
 
             COL_NAME="cba6f7"
             COL_DIR="f2cdcd"
@@ -102,10 +85,24 @@ in {
               unset COL_NAME COL_DIR COL_GIT
               source ~/.config/zsh/prompt.sh
             }
-            add-zsh-hook precmd getColors
+
+            # add-zsh-hook precmd getColors
+            # add-zsh-hook precmd vcs_info
+            precmd_functions+=(vcs_info getColors)
+
+            # Style the vcs_info message
+            zstyle ':vcs_info:*' enable git
+            zstyle ':vcs_info:git*' formats '%b%u%c'
+            # Format when the repo is in an action (merge, rebase, etc)
+            zstyle ':vcs_info:git*' actionformats '%F{14}⏱ %*%f'
+            zstyle ':vcs_info:git*' unstagedstr '*'
+            zstyle ':vcs_info:git*' stagedstr '+'
+            # This enables %u and %c (unstaged/staged changes) to work,
+            # but can be slow on large repos
+            zstyle ':vcs_info:*:*' check-for-changes true
 
 
-            PROMPT="''${NEWLINE}%F{#$COL_NAME}%n%f %F{#$COL_DIR}%3~%f %F{#$COL_GIT}''${vcs_info_msg_0_}%f %(?..%B%F{red}(%?%)%f%b)''${NEWLINE}> "
+            PROMPT=' ''${NEWLINE}%F{#$COL_NAME}%n%f %F{#$COL_DIR}%3~%f %F{#$COL_GIT}''${vcs_info_msg_0_}%f %(?..%B%F{red}(%?%)%f%b)''${NEWLINE}> '
 
             ${
               if config.modules.programs.themer.enable

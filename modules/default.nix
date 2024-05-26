5cf3af3fc71b346a983b6ae2c20abd5cf6a5d993
Lennart Koziollek <lennart.koziollek@stud.uni-due.de>
2024-05-26 22:36:38 +0200

    move themer from themes to themer dir

--- modules/default.nix
+++ modules/default.nix
@@ -50,7 +50,7 @@
     ./ssh
     ./nh
 
-    ./themes
+    ./themer
   ];
 
 
---
+++ modules/themer/default.nix
@@ -0,0 +1,59 @@
+{
+  inputs,
+  config,
+  lib,
+  pkgs,
+  ...
+}: let
+  cfg = config.modules.programs.themer;
+  username = config.modules.user.username;
+in {
+  options.modules.programs.themer = {
+    enable = lib.mkEnableOption "Enable themer";
+  };
+
+  config = lib.mkIf cfg.enable {
+    home-manager.users.${username} = {
+      home.packages = [
+        inputs.themer.packages.${pkgs.system}.default
+      ];
+      xdg.configFile = {
+        "themer/gruvbox.yaml".source = ./gruvbox.yaml;
+        "themer/catppuccin.yaml".source = ./catppuccin.yaml;
+        "themer/onedark.yaml".source = ./onedark.yaml;
+        "themer/rose-pine-dawn.yaml".source = ./rose-pine-dawn.yaml;
+        "themer/rose-pine-moon.yaml".source = ./rose-pine-moon.yaml;
+        # "themer/themer.toml".text = ''
+        #   [hyprland]
+        #   enable = true
+        #
+        #   [kitty]
+        #   enable = true
+        #
+        #   [nvim]
+        #   enable = true
+        #
+        #   [tmux]
+        #   enable = true
+        #
+        #   [zsh]
+        #   enable = true
+        #
+        #   [walker]
+        #   enable = true
+        #
+        #   [swww]
+        #   enable = true
+        #   wallpaper_dir = "/home/lk/Pictures/Wallpaper"
+        #
+        #   [presets]
+        #
+        #   [presets.glassy]
+        #   transparency = "blur"
+        #   colorscheme = "rose-pine-moon"
+        #   wallpaper = "pink-house-moon.jpg"
+        # '';
+      };
+    };
+  };
+}

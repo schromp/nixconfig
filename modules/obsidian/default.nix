a9e15e61a6e0af62d4095c31ea8d4b6a0095ade2
Lennart Koziollek <lennart.koziollek@stud.uni-due.de>
2024-07-27 18:29:26 +0200

    obsidian no more versioning

--- modules/obsidian/default.nix
+++ modules/obsidian/default.nix
@@ -2,16 +2,15 @@
   config,
   lib,
   pkgs,
-  nixpkgs,
   ...
 }:
-with lib; let
+let
   cfg = config.modules.programs.obsidian;
 in {
-  options.modules.programs.obsidian.enable = mkEnableOption "Enable obsidian";
+  options.modules.programs.obsidian.enable = lib.mkEnableOption "Enable obsidian";
 
-  config = mkIf cfg.enable {
-    nixpkgs.config.permittedInsecurePackages = lib.optional (pkgs.obsidian.version == "1.5.3") "electron-25.9.0";
+  config = lib.mkIf cfg.enable {
+    # nixpkgs.config.permittedInsecurePackages = lib.optional (pkgs.obsidian.version == "1.5.3") "electron-25.9.0";
     environment.systemPackages = with pkgs; [ obsidian ];
   };
 }

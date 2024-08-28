fd2702b70c65826a01b29058509b2c307c1222d5
Lennart Koziollek <lennart.koziollek@gmail.com>
2024-08-28 09:25:41 +0200

    wezterm: rename tab hotkey

--- modules/wezterm/wezterm.lua
+++ modules/wezterm/wezterm.lua
@@ -95,6 +95,21 @@ config.keys = {
 	},
 	{ key = "UpArrow", mods = "SHIFT", action = act.ScrollToPrompt(-1) },
 	{ key = "DownArrow", mods = "SHIFT", action = act.ScrollToPrompt(1) },
+	{
+		key = ",",
+		mods = "LEADER",
+		action = act.PromptInputLine({
+			description = "Enter new name for tab",
+			action = wezterm.action_callback(function(window, pane, line)
+				-- line will be `nil` if they hit escape without entering anything
+				-- An empty string if they just hit enter
+				-- Or the actual line of text they wrote
+				if line then
+					window:active_tab():set_title(line)
+				end
+			end),
+		}),
+	},
 }
 
 for i = 1, 8 do

4c00e6f8bce13aa78506e4e553b7c35db45bb7ba
Lennart Koziollek <lennart.koziollek@gmail.com>
2024-09-11 15:51:38 +0200

    added unstable input for nix darwin

--- flake.nix
+++ flake.nix
@@ -4,12 +4,14 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     nixpkgs-darwin.url = "github:NixOS/nixpkgs/nixpkgs-24.05-darwin";
+    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     nixpkgs-wayland.url = "github:nix-community/nixpkgs-wayland";
 
     nix-darwin = {
       url = "github:LnL7/nix-darwin";
       inputs.nixpkgs.follows = "nixpkgs-darwin";
     };
+
 
     home-manager = {
       url = "github:nix-community/home-manager";

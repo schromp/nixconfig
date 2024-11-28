0369587f4d79ed2f434be0d116fa0e2f553dbf24
Lennart Koziollek <lennart.koziollek@gmail.com>
2024-11-28 12:53:18 +0100

    zellij: darwin package

--- modules/home/zellij/zellij.nix
+++ modules/home/zellij/zellij.nix
@@ -13,7 +13,10 @@ in {
 
   config = lib.mkIf cfg.enable {
     # Track zellij git package until 0.41.0 is in nixpkgs
-    home.packages = [inputs.zellij-git.packages.${pkgs.system}.default];
+    home.packages =
+      if pkgs.system == "aarch64-darwin"
+      then [inputs.nixpkgs.legacyPackages."aarch64-darwin".zellij]
+      else [pkgs.zellij];
 
     xdg.configFile."zellij/config.kdl".text = builtins.readFile ./config.kdl;
     # xdg.configFile."zellij/layouts/default.kdl".text = builtins.readFile ./layouts/default.kdl;

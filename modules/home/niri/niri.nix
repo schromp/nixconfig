660887d2debf11b1a5e3a87df12736a875ef14e5
schromp <lennart@koziollek.com>
2025-10-13 15:02:20 +0200

    niri: smaller focus ring and brightness fix

--- modules/home/niri/niri.nix
+++ modules/home/niri/niri.nix
@@ -1,15 +1,22 @@
-{ osConfig, config, lib, pkgs, ... }:
+{
+  osConfig,
+  config,
+  lib,
+  pkgs,
+  ...
+}:
 let
   comp = osConfig.modules.local.system.compositor;
 in
 {
   config = lib.mkIf (comp == "niri") {
-    home.file.".config/niri/config.kdl".source = config.lib.file.mkOutOfStoreSymlink /home/lk/repos/nixconfig/modules/home/niri/config.kdl;
+    home.file.".config/niri/config.kdl".source =
+      config.lib.file.mkOutOfStoreSymlink /home/lk/repos/nixconfig/modules/home/niri/config.kdl;
 
     home.packages = with pkgs; [
       swww
+      brightnessctl
     ];
-
 
   };
 }
